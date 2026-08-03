23a6725aa20d47a358262d73809a9a9083dabb82
Ryan Riegel <ryan@riegelmedia.com>
2026-08-03 14:08:02 -0400

    add aws.virginia provider for CloudFront certificate requirements

--- infra/providers.tf
+++ infra/providers.tf
@@ -10,6 +10,20 @@ provider "aws" {
   }
 }
 
+# CloudFront requires certificates in us-east-1
+provider "aws" {
+  alias  = "virginia"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project     = "portfolio"
+      Environment = var.environment
+      ManagedBy   = "terraform"
+    }
+  }
+}
+
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
