399c6cb31799156aee3fa66456736b99192a049f
Ryan Riegel <ryan@riegelmedia.com>
2026-08-05 10:14:44 -0400

    add Cloudflare DNS records for CloudFront and API Gateway

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 module "static_site" {
   source = "./modules/static-site"
 
-  bucket_name     = "${var.project_name}-${var.environment}"
+  bucket_name     = "${var.project_name}-${var.environment}-rrigel"
   environment   = var.environment
   domain_name   = var.domain_name
   certificate_arn = aws_acm_certificate.main.arn
@@ -22,4 +22,32 @@ module "ses" {
   project_name = var.project_name
   environment  = var.environment
   domain_name  = var.domain_name
+}
+
+# Cloudflare DNS records for CloudFront
+resource "cloudflare_record" "site_root" {
+  zone_id = var.cloudflare_zone_id
+  name    = "ryanriegel.dev"
+  content = module.static_site.cloudfront_domain_name
+  type    = "CNAME"
+  proxied = false  # DNS only - CloudFront handles SSL/TLS
+  ttl     = 1
+}
+
+resource "cloudflare_record" "site_www" {
+  zone_id = var.cloudflare_zone_id
+  name    = "www"
+  content = module.static_site.cloudfront_domain_name
+  type    = "CNAME"
+  proxied = false  # DNS only - CloudFront handles SSL/TLS
+  ttl     = 1
+}
+
+resource "cloudflare_record" "site_api" {
+  zone_id = var.cloudflare_zone_id
+  name    = "api"
+  content = replace(module.api.api_endpoint, "https://", "")
+  type    = "CNAME"
+  proxied = false
+  ttl     = 1
 }
